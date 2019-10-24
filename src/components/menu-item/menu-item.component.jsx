6a548a79da3162735be3d1242589701912758412
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-10-24 11:03:29 +0400

    commit 4.11

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,11 +4,12 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({title, imageUrl, size}) => {
   return (
-    <div style={{
+    <div className={`${size} menu-item`}>
+      <div className='background-image' style={{
       backgroundImage: `url(${imageUrl})`
-    }} className={`${size} menu-item`}>
+    }}/>
       <div className="content">
-        <h2 className="title">{title}</h2>
+        <h2 className="title">{title.toUpperCase()}</h2>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
