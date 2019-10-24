2e411f6f4826a054865a218d4f25d543f09477a4
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-10-24 15:54:51 +0400

    commit 6.3

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import CollectionItem from '../collection-item/collection-item.component'
+
 import './collection-preview.styles.scss'
 
 const CollectionPreview = ({title, items}) => {
@@ -8,8 +10,8 @@ const CollectionPreview = ({title, items}) => {
       <h2 className="title">{title}</h2>
       <div className="preview">
         {
-          items.filter((item, idx) => idx < 4).map((item) => (
-            <div key={item.id}>{item.name}</div>
+          items.filter((item, idx) => idx < 4).map(({id, ...otherItemProps}) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))
         }
       </div>
