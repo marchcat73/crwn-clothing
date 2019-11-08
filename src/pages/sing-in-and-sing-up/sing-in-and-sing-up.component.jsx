8bf359c3ed398a63e70f0c8915b891a3d76bc0c9
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-11-08 17:35:52 +0400

    commit 6.7

---
+++ src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+import './sing-in-and-sing-up.styles.scss'
+
+const SignInAndSignUpPage = () => (
+    <div className="sing-in-and-sing-up">
+      <SignIn />
+    </div>
+  )
+
+
+export default SignInAndSignUpPage
