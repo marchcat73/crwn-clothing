2255138c7c4e2f43591c193b5ca549cce04f6be5
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-11-13 16:01:34 +0400

    commit 7.15

--- src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
+++ src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 import './sing-in-and-sing-up.styles.scss'
 
 const SignInAndSignUpPage = () => (
     <div className="sing-in-and-sing-up">
       <SignIn />
+      <SignUp />
     </div>
   )
 
