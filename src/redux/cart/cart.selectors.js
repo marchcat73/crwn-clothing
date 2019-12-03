59eac8e015c51f68a7afd6e4cd252dee4e79d82c
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-12-03 12:04:12 +0400

    commit 8.25

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -17,3 +17,12 @@ export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems => cartItems.reduce((accumaltedQuantity, cartItem) => accumaltedQuantity + cartItem.quantity, 0)
 )
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce(
+    (accumaltedQuantity, cartItem) => 
+      accumaltedQuantity + cartItem.quantity * cartItem.price, 
+      0
+    )
+)
