7ebfb674daa28360da1b26d9e3c9836977a3079c
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-12-08 19:42:30 +0400

    commit 8.30

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,7 +1,7 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   /** Проверяем, есть ли уже в стейте данный добавляемый в корзину товар */
   const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id)
-
+  console.log(cartItemToAdd)
   if (existingCartItem) {
     return cartItems.map(cartItem => 
       cartItem.id === cartItemToAdd.id 
@@ -11,4 +11,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  }
+
+  return cartItems.map(
+    cartItem => 
+    cartItem.id === cartItemToRemove.id ?
+    { ...cartItem, quantity: cartItem.quantity - 1 }
+    : cartItem
+  )
 }
