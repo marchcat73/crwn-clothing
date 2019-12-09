2ad94628b7c7a92b7cc1b02e4814c91af12e8343
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-12-09 17:46:47 +0400

    commit 10.1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
-import {createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
 
-import rootReducer from './root-reducer'
+import persistedReducer from './root-reducer'
 
 const middlewares = [logger]
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(persistedReducer, applyMiddleware(...middlewares))
 
-export default store
+export const persiststor = persistStore(store)
+
+export default { store, persiststor }
