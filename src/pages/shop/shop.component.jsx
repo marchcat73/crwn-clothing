93be82cbda9b9ce7f2b6a6eb1ed0bde6d11763b2
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-01-16 12:54:28 +0400

    commit 18.3

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,55 +1,48 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import { connect } from "react-redux";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap
-} from "../../firebase/firebase.utils";
-import { updateCollections } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
 
-import WithSpinner from "../../components/with-spinner/with-spnner.component";
+import WithSpinner from '../../components/with-spinner/with-spnner.component';
 
-import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import CollectionPage from "../collection/collection.component";
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-  state = {
-    loading: true
-  };
-
-  unsubscribeFromSnapshop = null;
-
   componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection("collections");
+    const { fetchCollectionsStartAsync } = this.props;
 
-    collectionRef.get().then(async snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({ loading: false });
-    });
+    fetchCollectionsStartAsync();
   }
 
   render() {
-    const { match } = this.props;
-    const { loading } = this.state;
+    const { match, isCollectionFetching } = this.props;
+
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
           render={props => (
-            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+            <CollectionsOverviewWithSpinner
+              isLoading={isCollectionFetching}
+              {...props}
+            />
           )}
         />
         <Route
           path={`${match.path}/:collectionId`}
           render={props => (
-            <CollectionPageWithSpinner isLoading={loading} {...props} />
+            <CollectionPageWithSpinner
+              isLoading={isCollectionFetching}
+              {...props}
+            />
           )}
         />
       </div>
@@ -57,9 +50,12 @@ class ShopPage extends React.Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  updateCollections: collectionsMap =>
-    dispatch(updateCollections(collectionsMap))
+const mapStateToProps = createStructuredSelector({
+  isCollectionFetching: selectIsCollectionFetching
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,21 @@
-import { createStore, applyMiddleware } from 'redux'
-import { persistStore } from 'redux-persist'
-import logger from 'redux-logger'
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
-import persistedReducer from './root-reducer'
+import persistedReducer from "./root-reducer";
 
-const middlewares = []
+const middlewares = [thunk];
 
-if (process.NODE_ENV === 'development') {
-  middlewares.push(logger)
+if (process.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 
-export const store = createStore(persistedReducer, applyMiddleware(...middlewares))
+export const store = createStore(
+  persistedReducer,
+  applyMiddleware(...middlewares)
+);
 
-export const persiststor = persistStore(store)
+export const persiststor = persistStore(store);
 
-export default { store, persiststor }
+export default { store, persiststor };
