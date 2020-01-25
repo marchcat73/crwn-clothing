c42f8bd34785c64f5c5ab24c10020e8d1d0244f7
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-01-25 13:32:36 +0400

    commit 20.4

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
-import { persistStore } from "redux-persist";
-import logger from "redux-logger";
-import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 
-import persistedReducer from "./root-reducer";
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
-const middlewares = [thunk];
+import persistedReducer from './root-reducer';
 
-if (process.NODE_ENV === "development") {
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+if (process.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
@@ -15,6 +19,8 @@ export const store = createStore(
   persistedReducer,
   applyMiddleware(...middlewares)
 );
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persiststor = persistStore(store);
 
