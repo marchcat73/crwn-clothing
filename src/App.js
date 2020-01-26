df684b351abb004337044aba308020e8962dd105
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-01-26 15:54:34 +0400

    commit 20.12

--- src/App.js
+++ src/App.js
@@ -10,27 +10,14 @@ import SignInAndSignUpPage from './pages/sing-in-and-sing-up/sing-in-and-sing-up
 import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    // const { setCurrentUser } = this.props;
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(
-    //   async userAuth => {
-    //     if (userAuth) {
-    //       const userRef = await createUserProfileDocument(userAuth);
-    //       userRef.onSnapshot(snapShot => {
-    //         setCurrentUser({
-    //           id: snapShot.id,
-    //           ...snapShot.data()
-    //         });
-    //       });
-    //     }
-    //     setCurrentUser(userAuth);
-    //   },
-    //   error => console.log(error)
-    // );
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() {
@@ -66,4 +53,8 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  checkUserSession: () => dispatch(checkUserSession())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
